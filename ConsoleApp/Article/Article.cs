98ddd755029dd012a5c5b08c5f39d1e50e8a6256
Jean-Baptiste Da Costa <jeanbapti.dacosta@edu.itescia.fr>
2020-09-22 11:47:59 +0200

    exo article

---
+++ ConsoleApp/Article/Article.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Article
+{
+    public struct Article
+    {
+        public Article(string nom, double prix, int quantite)
+        {
+            Nom = nom;
+            Prix = prix;
+            Quantite = quantite;
+        }
+
+        public string Nom { get; }
+        public double Prix { get; }
+        public int Quantite { get; set; }
+
+        public override string ToString() => $"Nom : {Nom}, Prix : {Prix}, Quantite : {Quantite}";
+
+        public void Afficher()
+        {
+            Console.WriteLine(this.ToString());
+        }
+
+        public void Ajouter(int add)
+        {
+            Quantite += add;
+        }
+
+        public void Retirer(int remove)
+        {
+            Quantite -= remove;
+        }
+    }
+}
---
+++ ConsoleApp/Article/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Article
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Article a1 = new Article("test", 10, 2);
+            Article a2 = new Article("test2", 10, 15);
+
+            a1.Afficher();
+            a2.Afficher();
+
+            a1.Ajouter(10);
+            a1.Afficher();
+
+            a2.Retirer(10);
+            a2.Afficher();
+
+            Console.Write("Saisir nom nouvelle article : ");
+            var nom = Console.ReadLine();
+            Console.Write("Saisir prix nouvelle article : ");
+            var prix = Console.ReadLine();
+            Console.Write("Saisir quantite nouvelle article : ");
+            var quantite = Console.ReadLine();
+
+            Article a3 = new Article(nom, Convert.ToInt32(prix), Convert.ToInt32(quantite));
+            a3.Afficher();
+
+            Console.ReadKey();
+        }
+    }
+}
